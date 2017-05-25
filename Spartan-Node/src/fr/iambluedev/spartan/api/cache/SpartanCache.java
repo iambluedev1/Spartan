bb1ac249367804d996cbceccaa0b7c6a8955b264
Iambluedev <iambluedev@gmx.fr>
2017-05-25 17:40:26 +0200

    cache

--- Spartan-Node/src/fr/iambluedev/spartan/api/cache/SpartanCache.java
+++ Spartan-Node/src/fr/iambluedev/spartan/api/cache/SpartanCache.java
@@ -1,9 +1,14 @@
 package fr.iambluedev.spartan.api.cache;
 
+import java.util.Map;
+
 public abstract class SpartanCache {
 
-	public SpartanCache(){
-		
-	}
+	public abstract Map<String, SpartanGameCache> getGameModes();
 	
+	public abstract SpartanGameCache getCache(String name);
+	
+	public abstract void addGameMode(String name, SpartanGameCache cache);
+	
+	public abstract void clearCache();
 }
