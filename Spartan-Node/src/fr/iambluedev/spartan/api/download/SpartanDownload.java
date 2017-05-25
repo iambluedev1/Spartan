99c50751ae38207326430228bb27a1a4cb4b14c6
Iambluedev <iambluedev@gmx.fr>
2017-05-25 21:57:37 +0200

    downloader sys

---
+++ Spartan-Node/src/fr/iambluedev/spartan/api/download/SpartanDownload.java
@@ -0,0 +1,96 @@
+package fr.iambluedev.spartan.api.download;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import fr.iambluedev.spartan.api.http.SpartanUrl;
+
+public class SpartanDownload extends Thread{
+
+	private SpartanUrl spartanUrl;
+	
+	public SpartanDownload(SpartanUrl spartanUrl){
+		this.spartanUrl = spartanUrl;
+	}
+
+	@Override
+	public void run(){
+		URL url = null;
+		try {
+			url = new URL(this.getSpartanUrl().getUrl());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+        HttpsURLConnection httpConn = null;
+		try {
+			httpConn = (HttpsURLConnection) url.openConnection();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        int responseCode = 0;
+        
+		try {
+			responseCode = httpConn.getResponseCode();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+ 
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            InputStream inputStream = null;
+			try {
+				inputStream = httpConn.getInputStream();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+            String saveFilePath = this.getSpartanUrl().getDest();
+             
+            FileOutputStream outputStream = null;
+			try {
+				outputStream = new FileOutputStream(saveFilePath);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+ 
+            int bytesRead = -1;
+            byte[] buffer = new byte[8192];
+            
+            try {
+				while ((bytesRead = inputStream.read(buffer)) != -1) {
+				    outputStream.write(buffer, 0, bytesRead);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+ 
+            try {
+				outputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+            
+            try {
+				inputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+ 
+            System.out.println("[" + this.getSpartanUrl().getName() + "] File downloaded to : " + this.getSpartanUrl().getDest());
+        } else {
+            System.out.println("[" + this.getSpartanUrl().getName() + "] Error : Server replied HTTP code: " + responseCode);
+        }
+        httpConn.disconnect();
+	}	
+	
+	public SpartanUrl getSpartanUrl() {
+		return this.spartanUrl;
+	}
+}
--- Spartan-Node/src/fr/iambluedev/spartan/api/http/SpartanUrl.java
+++ Spartan-Node/src/fr/iambluedev/spartan/api/http/SpartanUrl.java
@@ -1,5 +1,30 @@
 package fr.iambluedev.spartan.api.http;
 
-public abstract class SpartanUrl {
+public class SpartanUrl {
 
+	private String url;
+	private String dest;
+	private String name;
+	
+	public SpartanUrl(String url, String name){
+		this.url = url;
+		this.dest = "";
+	}
+	
+	public SpartanUrl(String url, String dest, String name){
+		this.url = url;
+		this.dest = dest;
+	}
+
+	public String getUrl() {
+		return this.url;
+	}
+
+	public String getDest() {
+		return this.dest;
+	}
+
+	public String getName() {
+		return this.name;
+	}
 }
