dfeaa50a0811f78247f5a0b133f828c4072d2ccf
Iambluedev <iambluedev@gmx.fr>
2017-05-27 21:20:57 +0200

    finishing cache sys

--- Spartan-Node/src/fr/iambluedev/spartan/node/managers/CacheManager.java
+++ Spartan-Node/src/fr/iambluedev/spartan/node/managers/CacheManager.java
@@ -3,21 +3,27 @@ package fr.iambluedev.spartan.node.managers;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
 
 import fr.iambluedev.spartan.api.cache.SpartanCache;
 import fr.iambluedev.spartan.api.cache.SpartanGameCache;
 import fr.iambluedev.spartan.api.gamemode.SpartanGameMode;
+import fr.iambluedev.spartan.api.node.SpartanNode;
+import fr.iambluedev.spartan.api.utils.IOUtils;
 
 public class CacheManager extends SpartanCache{
 
 	private Map<String, SpartanGameMode> gamemodes;
 	private File folder;
+	private SpartanNode instance;
 	
-	public CacheManager(){
+	public CacheManager(SpartanNode instance){
 		this.gamemodes = new HashMap<String, SpartanGameMode>();
 		this.folder = new File("cache");
+		this.instance = instance;
 		if(this.folder.exists()){
-			this.folder.delete();
+			this.instance.getLogger().log(Level.INFO, "Deleting cache folder");
+			IOUtils.deleteDir(this.folder);
 		}
 		
 		this.folder.mkdir();
@@ -52,5 +58,10 @@ public class CacheManager extends SpartanCache{
 	public File getFolder() {
 		return this.folder;
 	}
+
+	public SpartanNode getInstance() {
+		return this.instance;
+	}
+	
 	
 }
