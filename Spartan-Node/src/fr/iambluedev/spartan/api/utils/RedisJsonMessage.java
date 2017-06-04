7bae894141b0af7ed24011dd1aabb91d574b2f02
Iambluedev <iambluedev@gmx.fr>
2017-06-04 18:54:46 +0200

    add util class

---
+++ Spartan-Node/src/fr/iambluedev/spartan/api/utils/RedisJsonMessage.java
@@ -0,0 +1,31 @@
+package fr.iambluedev.spartan.api.utils;
+
+import fr.iambluedev.spartan.api.gson.JSONObject;
+import fr.iambluedev.spartan.node.Main;
+
+public class RedisJsonMessage {
+
+	private JSONObject jsonObj;
+	
+	@SuppressWarnings("unchecked")
+	public RedisJsonMessage(){
+		this.jsonObj = new JSONObject();
+		this.jsonObj.put("id", Main.getInstance().getName() + "-" + Main.getInstance().getId());
+	}
+	
+	@SuppressWarnings("unchecked")
+	public RedisJsonMessage setCmd(String cmd){
+		this.jsonObj.put("cmd", cmd);
+		return this;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public RedisJsonMessage setMessage(String message){
+		this.jsonObj.put("message", message);
+		return this;
+	}
+	
+	public String get(){
+		return this.jsonObj.toJSONString();
+	}
+}
